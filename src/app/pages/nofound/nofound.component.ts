cc74a9f29de932d1cdc1065bd914dd7a09b6547e
hmunozd <jildo.co@gmail.com>
2022-03-27 23:42:17 -0500

    fin seccion 3 - diseño

--- src/app/pages/nofound/nofound.component.ts
+++ src/app/pages/nofound/nofound.component.ts
@@ -3,11 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nofound',
   templateUrl: './nofound.component.html',
-  styles: [
-  ]
+  styleUrls: ['./nofound.component.css']
 })
 export class NofoundComponent implements OnInit {
-
+  year = new Date().getFullYear();
   constructor() { }
 
   ngOnInit(): void {
